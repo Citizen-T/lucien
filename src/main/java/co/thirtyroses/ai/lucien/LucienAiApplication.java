291075f5b733df11510e162bf0be8d7d44c5ac24
Mike Triantafelow <mtriantafelow@gmail.com>
2025-08-14 00:08:38 -0400

    Initial commit

---
+++ src/main/java/co/thirtyroses/ai/lucien/LucienAiApplication.java
@@ -0,0 +1,13 @@
+package co.thirtyroses.ai.lucien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucienAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucienAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/thirtyroses/ai/lucien/LucienAiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.thirtyroses.ai.lucien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucienAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
